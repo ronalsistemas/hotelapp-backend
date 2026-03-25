a473e666cb89c1ffd1d60993e0b0d5fe6b71c2c7
Ronal Casani <ronalsistemas@gmail.com>
2026-03-24 23:07:46 -0500

    proyecto_base hotelapp_backend

---
+++ src/main/java/com/rcasani/HotelappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rcasani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelappBackendApplication.class, args);
+    }
+
+}
